5eab0a3f77b6e8460fed954afc4690ebd082ff69
gdgagan696 <gagan.dhand@non.se.com>
2020-08-18 14:36:08 +0530

    Adjusting Indentation.

--- src/dsquestions/ArraySubset.java
+++ src/dsquestions/ArraySubset.java
@@ -4,31 +4,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ArraySubset {
-	
-	private static List<List<Integer>> findSubset(int []arr) {
-		
-		List<List<Integer>> result=new ArrayList<>();
+
+	private static List<List<Integer>> findSubset(int[] arr) {
+
+		List<List<Integer>> result = new ArrayList<>();
 		result.add(new ArrayList<Integer>());
-		for(int x:arr) {
-			int n=result.size();
-			for(int i=0;i<n;i++) {
-				ArrayList<Integer> temp=new ArrayList<>(result.get(i));
+		for (int x : arr) {
+			int n = result.size();
+			for (int i = 0; i < n; i++) {
+				ArrayList<Integer> temp = new ArrayList<>(result.get(i));
 				temp.add(x);
 				result.add(temp);
 			}
 		}
 		return result;
-		
-		
 	}
 
 	public static void main(String[] args) {
-		int arr[] = new int[] { 1, 2, 3 ,4 };
-		List<List<Integer>> result=findSubset(arr);
+		int arr[] = new int[] { 1, 2, 3, 4 };
+		List<List<Integer>> result = findSubset(arr);
 		for (int i = 0; i < result.size(); i++) {
 			System.out.println(result.get(i));
 		}
-
 	}
 
 }
